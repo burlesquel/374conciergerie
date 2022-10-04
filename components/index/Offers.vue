2ee20fa4214c93ec04f4d5e49adc91093f229db2
burlesquel <batumanav@gmail.com>
2022-10-04 20:48:02 +0300

    services carousel

--- components/index/Offers.vue
+++ components/index/Offers.vue
@@ -6,7 +6,7 @@ export default {
     }
 }
 </script>
-
+    
 <template>
     <section class="flex flex-col p-4 gap-8 items-center bg-gray-100">
         <div>
@@ -14,11 +14,15 @@ export default {
             <div class="h-0.5 bg-logorange"></div>
         </div>
         <div class="flex flex-row justify-evenly w-full flex-wrap">
-            <Offer header="Daily" image_uri="/sea-voyage-to-bosphorus-strait.webp" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam id vitae illo quam ipsa odio necessitatibus deleniti saepe ut animi!"/>
-            <Offer header="Weekend" image_uri="/beautiful-travaler-with-backpack-on-the-platform.webp" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam id vitae illo quam ipsa odio necessitatibus deleniti saepe ut animi!"/>
-            <Offer header="Weekly" image_uri="/business-people-arriving-at-hotel.webp" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam id vitae illo quam ipsa odio necessitatibus deleniti saepe ut animi!"/>
-            <Offer header="Custom" image_uri="/top-view-of-workplace-woman-marks-dates-on-calenda.webp" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam id vitae illo quam ipsa odio necessitatibus deleniti saepe ut animi!"/>
-            
+            <Offer header="Daily" image_uri="/sea-voyage-to-bosphorus-strait.webp"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam id vitae illo quam ipsa odio necessitatibus deleniti saepe ut animi!" />
+            <Offer header="Weekend" image_uri="/beautiful-travaler-with-backpack-on-the-platform.webp"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam id vitae illo quam ipsa odio necessitatibus deleniti saepe ut animi!" />
+            <Offer header="Weekly" image_uri="/business-people-arriving-at-hotel.webp"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam id vitae illo quam ipsa odio necessitatibus deleniti saepe ut animi!" />
+            <Offer header="Custom" image_uri="/top-view-of-workplace-woman-marks-dates-on-calenda.webp"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam id vitae illo quam ipsa odio necessitatibus deleniti saepe ut animi!" />
+
         </div>
     </section>
 </template>
--- components/services/landing.vue
+++ components/services/landing.vue
@@ -1,13 +1,40 @@
-<template>
-    <section class="p-4">
-        <h1 class="animation-element text-5xl sm:text-6xl md:text-7xl lg:text-8xl fade-up text-logorange text-center">
-            SERVICES</h1>
-        <!-- <VueSlickCarousel class="p-4 m-4 border border-gray-300 shadow-lg rounded bg-white" :autoplay="true"
-            :arrows="true" :dots="false">
-            <div v-for="">
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
+export default {
+    name: 'MyComponent',
+    components: { VueSlickCarousel },
+    data() {
+        return {
+            images: [
+                "dentist",
+                "empty-football-stadium",
+                "happy-romantic-couple-moving-away-from-private-hel",
+                "helicopter-flying-overhead-against-blue-sky-and-wh",
+                "hotel-room-or-apartment-doorway",
+                "luxurious-yacht-illuminated",
+                "main-in-car",
+                "restaurant",
+                "young-woman-touching-turkish-lamps-for-sale-in-the"
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <section class="flex flex-col items-center pb-4 bg-gray-200 h-70vh sm:h-90vh w-full justify-around">
+        <h1 class="animation-element fade-right text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-light fade-up text-logorange text-center">
+            SERVICES</h1>
+        <VueSlickCarousel class="w-full flex flex-row items-center h-4/5 sm:h-3/4 sm:w-2/3"
+            :autoplay="true" :arrows="true" :autoplaySpeed="2000">
+            <div v-for="image in images" :key="image" class=" flex items-center justify-center bg-red-300">
+                <nuxt-img class="object-contain h-full w-full"  :src="`/carousel/${image}.webp`" />
             </div>
-        </VueSlickCarousel> -->
+        </VueSlickCarousel>
     </section>
 </template>
 
