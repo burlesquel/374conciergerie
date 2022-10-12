ff620c5555280b0d4b3c8cab8e55273f2382d92a
burlesquel <batumanav@gmail.com>
2022-10-12 22:10:19 +0300

    new landing video

--- components/index/Landing.vue
+++ components/index/Landing.vue
@@ -25,29 +25,29 @@ export default {
 </script>
 
 <template>
-    <section class="relative w-full bg-gray-100">
+    <section class="relative w-full h-90vh bg-gray-100">
         <!-- <Header /> -->
         <video v-if="screenSize.width > 640"
-            class="hidden sm:flex h-screen w-full z-1 bg-gray-300 object-cover object-center absolute left-0 right-0 top-0 bottom-0"
-            autoplay muted loop playsinline id="myVideo">
-            <source src="~/assets/entrance_video.mp4" type="video/mp4">
+            class="hidden sm:flex h-wfull w-full bg-gray-300 object-cover object-bottom"
+            autoplay playsinline id="myVideo">
+            <source src="~/assets/entrance_video.webm" type="video/webm">
         </video>
+
         <nuxt-img v-if="screenSize.width < 640"
-            class="h-screen w-full z-1 flex sm:hidden object-cover object-center absolute left-0 right-0 top-0 bottom-0"
+            class="h-full w-full object-cover object-center absolute"
             src="/landing_image.png" />
-        <main class="w-full h-screen flex relative flex-col">
 
-
+        <div v-if="screenSize.width < 640" class="w-full h-full z-10 relative">
             <div class=" h-full z-1 text-white flex items-start flex-col justify-around p-8 ">
-                <div class="flex w-2/3 flex-col gap-8">
-                    <h1 class="heading text-5xl sm:text-6xl md:text-7xl bold">374 Conciergerie: Your personal concierge service in Istanbul.</h1>
-                    <h2 class="subheading text-2xl sm:text-3xl md:text-4xl bold">
+                <div class="flex w-3/4 flex-col gap-8">
+                    <h1 class="heading text-4xl sm:text-5xl md:text-6xl bold">374 Conciergerie: Your personal concierge service in Istanbul.</h1>
+                    <!-- <h2 class="subheading text-2xl sm:text-3xl md:text-4xl bold">
 For a few days or a long time, for vacation or work, 374 Conciergerie makes your life easier.
-</h2>
+</h2> -->
                 </div>
                 <button class="text-2xl p-2 bg-yellow-600 inline text-gray-900 rounded">Book now</button>
             </div>
-        </main>
+        </div>
     </section>
 </template>
 
--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,19 @@
 <script>
-    import Animatable from '../components/animation/animatable.vue';
+import Animatable from '../components/animation/animatable.vue';
 import Landing from '../components/about/Landing.vue';
 import WhoAreWe from '../components/about/WhoAreWe.vue';
 import Advantages from '../components/about/Advantages.vue';
 import ReachUsForm from '../components/ReachUsForm.vue';
-    export default{
-    components:{ Animatable, Landing, WhoAreWe, Advantages, ReachUsForm }
+export default {
+    components: { Animatable, Landing, WhoAreWe, Advantages, ReachUsForm }
 }
 </script>
 
 <template>
     <Animatable>
-        <Landing/>
-        <WhoAreWe/>
-        <Advantages/>
-        <ReachUsForm/>
+        <Landing />
+        <WhoAreWe />
+        <Advantages />
+        <ReachUsForm />
     </Animatable>
 </template>
