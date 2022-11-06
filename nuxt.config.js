c85ea5b7144ccafc4dfb8fa74ce322ca5f51f6e9
burlesquel <batumanav@gmail.com>
2022-11-06 18:07:33 +0300

    head removed from nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,12 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '374conciergerie',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
