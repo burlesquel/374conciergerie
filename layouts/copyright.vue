f056b2f048486639f3f298411df7bdbbe6caa74a
burlesquel <batumanav@gmail.com>
2023-02-11 06:35:16 +0300

    copyright batuhanmanav

--- layouts/copyright.vue
+++ layouts/copyright.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="bg-black text-white text-center">
-        © 2022 374Conciergerie | All rights reserved.
+        Designed by <a href="https://batuhanmanav.vercel.app">Batuhan Manav</a>© 2022 374Conciergerie | All rights reserved.
     </div>
 </template>
